50918e1f94b0ee8bd2547a5411b05ac75ba0d236
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-01-10 15:49:13 -0600

    User can change location now

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,10 +1,32 @@
 import Header from "./Header";
 import SideBar from "./SideBar";
+import { useEffect, useState } from "react";
 
 export default function Content() {
+  const [location, setLocation] = useState("Dolores Hidalgo");
+
+  useEffect(() => {
+    async function getWeather(location) {
+      try {
+        let getForecast = await fetch(
+          "https://api.weatherapi.com/v1/forecast.json?key=fdd09a79a4d5440a8ad165337231011&q=" +
+            location,
+          { mode: "cors" }
+        );
+        let forecast = await getForecast.json();
+        displayWeather(forecast);
+      } catch (error) {
+        console.log("Something happen " + error);
+      }
+    }
+  }, [location]);
+
+  function handleLocation(loc) {
+    setLocation(loc);
+  }
   return (
     <div className="main">
-      <Header city={"City you looked for"} />
+      <Header city={location} handleChange={handleLocation} />
       <div className="content">
         <SideBar />
       </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,38 @@
 import search from "../assets/buscar.png";
 import sun from "../assets/sun.png";
+import map from "../assets/map.png";
+import { useState } from "react";
 
-export default function Header({ city }) {
+export default function Header({ city, handleChange }) {
+  const [value, setValue] = useState("");
+
+  function handlePress(event) {
+    if (event.key === "Enter") {
+      handleChange(value);
+    }
+  }
+
+  function handleValue(event) {
+    setValue(event.target.value);
+  }
+
   return (
     <div className="header">
       <img src={sun} alt="" className="logoImg" />
-      <div>{city}</div>
+      <div style={{ display: "flex", gap: ".5rem", alignItems: "center" }}>
+        <img src={map} alt="hola" className="searchImg" />
+        <div>{city}</div>
+      </div>
       <div className="input">
         <img src={search} alt="" className="searchImg" />
-        <input type="text" placeholder="Search" />
+        <input
+          type="text"
+          placeholder="Search"
+          value={value}
+          onChange={handleValue}
+          onKeyDown={handlePress}
+        />
       </div>
-      {/* </input>s */}
     </div>
   );
 }
