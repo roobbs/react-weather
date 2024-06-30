497a79a9d2c65bdd81586345714108188f5ccf9b
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-06-29 18:49:46 -0600

    Added new style and icons to sidebar component

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,11 +1,17 @@
 import "../styles/SideBar.css";
 import { LuSunrise, LuSunset } from "react-icons/lu";
+import { TbUvIndex } from "react-icons/tb";
+import { WiRain } from "react-icons/wi";
+import { WiStrongWind } from "react-icons/wi";
+import { WiThermometer } from "react-icons/wi";
+import { WiCloudy } from "react-icons/wi";
+import { WiHumidity } from "react-icons/wi";
 
 export default function SideBar(props) {
   return (
     <div className="sideBar">
       <div>
-        Current Weather{" "}
+        <div className="sidebarTitle">Current Weather </div>
         <div className="currentHourNumber">{props.hour} hrs</div>
       </div>
       <div className="currentInfo">
@@ -15,22 +21,45 @@ export default function SideBar(props) {
       </div>
       <div className="generalInfo">
         <div className="smallInfo">
-          Feels Like: <p>{props.feels} C°</p>
+          <div className="iconContainer">
+            <WiThermometer size={23} className="currentWeatherIcon" />
+            Feels Like:
+          </div>{" "}
+          <p>{props.feels} C°</p>
         </div>
         <div className="smallInfo">
-          Humidity: <p>{props.humidity} %</p>
+          <div className="iconContainer">
+            <WiHumidity size={23} className="currentWeatherIcon" />
+            Humidity:
+          </div>
+          <p>{props.humidity} %</p>
         </div>
         <div className="smallInfo">
-          Clouds: <p>{props.clouds} %</p>
+          <div className="iconContainer">
+            <WiCloudy size={23} className="currentWeatherIcon" />
+            Clouds:
+          </div>
+          <p>{props.clouds} %</p>
         </div>
         <div className="smallInfo">
-          Rain: <p>{props.rain} %</p>
+          <div className="iconContainer">
+            <WiRain size={23} className="currentWeatherIcon" /> Rain:
+          </div>{" "}
+          <p>{props.rain} %</p>
         </div>
         <div className="smallInfo">
-          Wind: <p>{props.wind} k/h</p>
+          <div className="iconContainer">
+            <WiStrongWind size={23} className="currentWeatherIcon" />
+            Wind:
+          </div>{" "}
+          <p>{props.wind} k/h</p>
         </div>
         <div className="smallInfo">
-          UV Level: <p>{props.uv}</p>
+          <div className="iconContainer">
+            <TbUvIndex size={23} className="currentWeatherIcon" />
+            UV Level:
+          </div>
+          <p>{props.uv}</p>
         </div>
       </div>
       <div className="sun">
