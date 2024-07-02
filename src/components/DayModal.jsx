1644e5ed063d527034108a6ce78a1c6258c097e1
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-07-01 21:59:05 -0600

    Rearrange elements in Modal and added new date

--- src/components/DayModal.jsx
+++ src/components/DayModal.jsx
@@ -10,88 +10,99 @@ import { WiHumidity } from "react-icons/wi";
 import { LuSunrise, LuSunset } from "react-icons/lu";
 
 export default function DayModal({ onClose, info }) {
+  const firstDay = info.date;
+  let day = firstDay.split("-");
+  const today = new Date().getDate();
+  if (today === day[2] - 1) {
+    day = `Tomorrow (${info.date})`;
+  } else {
+    day = firstDay;
+  }
+
   return (
-    <div className="modalContainer">
+    <div>
       <div className="closeIcon" onClick={onClose}>
         <RiArrowGoBackLine size={30} />
         Go back
       </div>
-      <div className="dayModalContent">
-        <div className="modalDayTitle">{info.date} </div>
-        <div className="modalCondition">
-          <div className="minMaxTemp">
-            <img src={info.day.condition.icon} alt="weatherIcon" />
-            <div style={{ display: "flex" }}>
-              <div className="modalMaxTemp">
-                <WiDirectionUp size={40} className="modalIcon" />
-                {info.day.maxtemp_c} C°
+      <div className="modalContainer">
+        <div className="dayModalContent">
+          <div className="modalDayTitle">{day}</div>
+          <div className="modalCondition">{info.day.condition.text}</div>
+          <div className="modalCondition">
+            <div className="minMaxTemp">
+              <img src={info.day.condition.icon} alt="weatherIcon" />
+              <div style={{ display: "flex" }}>
+                <div className="modalMaxTemp">
+                  <WiDirectionUp size={40} className="modalIcon" />
+                  {info.day.maxtemp_c} C°
+                </div>
+                <div className="modalMinTemp">
+                  <WiDirectionDown size={40} className="modalIcon" />
+                  {info.day.mintemp_c} C°
+                </div>
               </div>
-              <div className="modalMinTemp">
-                <WiDirectionDown size={40} className="modalIcon" />
-                {info.day.mintemp_c} C°
+            </div>
+          </div>
+          <div className="modalSmallInfoContainer">
+            <div className="info">
+              <div className="modalInfoIcon">
+                <WiHumidity size={24} className="modalIcon" /> Humidity:
+              </div>
+              <div className="modalInfoNumber">{info.day.avghumidity}% </div>
+            </div>
+            <div className="info">
+              <div className="modalInfoIcon">
+                <TbUvIndex size={24} className="modalIcon" />
+                UV level:
+              </div>
+              <div className="modalInfoNumber">{info.day.uv} </div>
+            </div>
+            <div className="info">
+              <div className="modalInfoIcon">
+                <WiRain size={24} className="modalIcon" />
+                Chance of rain:
+              </div>
+              <div className="modalInfoNumber">
+                {info.day.daily_chance_of_rain}%
               </div>
             </div>
-          </div>
-          <div>{info.day.condition.text}</div>
-        </div>
-        <div className="modalSmallInfoContainer">
-          <div className="info">
-            <div className="modalInfoIcon">
-              <WiHumidity size={24} className="modalIcon" /> Humidity:
-            </div>
-            <div className="modalInfoNumber">{info.day.avghumidity}% </div>
-          </div>
-          <div className="info">
-            <div className="modalInfoIcon">
-              <TbUvIndex size={24} className="modalIcon" />
-              UV level:
-            </div>
-            <div className="modalInfoNumber">{info.day.uv} </div>
-          </div>
-          <div className="info">
-            <div className="modalInfoIcon">
-              <WiRain size={24} className="modalIcon" />
-              Chance of rain:
-            </div>
-            <div className="modalInfoNumber">
-              {info.day.daily_chance_of_rain}%
+            <div className="info">
+              <div className="modalInfoIcon">
+                <WiStrongWind size={24} className="modalIcon" />
+                Max. wind:
+              </div>
+              <div className="modalInfoNumber">{info.day.maxwind_kph} k/h </div>
             </div>
           </div>
-          <div className="info">
-            <div className="modalInfoIcon">
-              <WiStrongWind size={24} className="modalIcon" />
-              Max. wind:
+          <div className="sunInfoContainer">
+            <div className="sunInfo">
+              <LuSunrise size={23} />
+              <div className="hourInfoNumber">{info.astro.sunrise}</div>
             </div>
-            <div className="modalInfoNumber">{info.day.maxwind_kph} k/h </div>
+            <div className="sunInfo">
+              <LuSunset size={23} />
+              <div className="hourInfoNumber">{info.astro.sunset}</div>
+            </div>
           </div>
         </div>
-        <div className="sunInfoContainer">
-          <div className="sunInfo">
-            <LuSunrise size={23} />
-            <div className="hourInfoNumber">{info.astro.sunrise}</div>
-          </div>
-          <div className="sunInfo">
-            <LuSunset size={23} />
-            <div className="hourInfoNumber">{info.astro.sunset}</div>
-          </div>
-        </div>
-      </div>
 
-      <div className="modalHourContainer">
-        {info.hour.map((hr, index) => (
-          <HourCard
-            key={index}
-            num={index}
-            img={hr.condition.icon}
-            condition={hr.condition.text}
-            temp={hr.temp_c}
-            rain={hr.chance_of_rain}
-            wind={hr.wind_kph}
-            cloud={hr.cloud}
-            humidity={hr.humidity}
-            uv={hr.uv}
-          />
-        ))}
+        <div className="modalHourContainer">
+          {info.hour.map((hr, index) => (
+            <HourCard
+              key={index}
+              num={index}
+              img={hr.condition.icon}
+              condition={hr.condition.text}
+              temp={hr.temp_c}
+              rain={hr.chance_of_rain}
+              wind={hr.wind_kph}
+              cloud={hr.cloud}
+              humidity={hr.humidity}
+              uv={hr.uv}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
