26b3578a66856b4cd020104fcf77967976383ae0
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-07-02 17:57:19 -0600

    Created Loader.jsx

---
+++ src/components/Loader.jsx
@@ -0,0 +1,10 @@
+import "../styles/Loader.css";
+
+export default function Loader() {
+  return (
+    <div className="loaderContainer">
+      <div className="loaderText">Wait...</div>
+      <div className="loader"></div>
+    </div>
+  );
+}
