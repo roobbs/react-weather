a33424c42447b613abb4e9e6b4ae6960fcebba46
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-07-21 04:49:38 -0600

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
-import Footer from "./components/Footer";
-import Content from "./components/Content";
+import Footer from "./components/Footer.tsx";
+import Content from "./components/Content.tsx";
 
 export default function App() {
   return (
